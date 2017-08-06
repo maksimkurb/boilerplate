bab7e4c780f42798dc8483360252730ddea4c82b
Maxim Kurbatov <maksimkurb@gmail.com>
2017-08-07 00:36:07 +0400

    Empty argument handled as directory and threw errors

--- tools/lint.js
+++ tools/lint.js
@@ -3,8 +3,8 @@ const childProcess = require('./childProcess');
 const constants = require('./constants');
 
 async function lint() {
-  const colorsArg = constants.USE_STDOUT_COLORS ? '--color' : '';
-  await childProcess(path.resolve(__dirname, '..', 'node_modules/.bin/eslint'), [colorsArg, constants.SOURCES_DIR]);
+  const colorsArg = constants.USE_STDOUT_COLORS ? '--color' : '--no-color';
+  await childProcess(path.resolve(__dirname, '..', 'node_modules/.bin/eslint'), [constants.SOURCES_DIR, colorsArg]);
 }
 
 module.exports = lint;
--- tools/test.js
+++ tools/test.js
@@ -3,8 +3,11 @@ const childProcess = require('./childProcess');
 const constants = require('./constants');
 
 async function test() {
-  const colorsArg = constants.USE_STDOUT_COLORS ? '--colors' : '';
-  await childProcess(path.resolve(__dirname, '..', 'node_modules/.bin/jest'), [colorsArg, path.resolve(__dirname, '..', constants.SOURCES_DIR)]);
+  const args = [path.resolve(__dirname, '..', constants.SOURCES_DIR)];
+  if (constants.USE_STDOUT_COLORS) {
+    args.push('--colors');
+  }
+  await childProcess(path.resolve(__dirname, '..', 'node_modules/.bin/jest'), args);
 }
 
 module.exports = test;
